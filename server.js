55fe169006467658e0c57841bbd6d292601c48f5
Kasumi <105487011+akama-aka@users.noreply.github.com>
2026-09-01 11:55:10 +0100

    Added UTF-8 Charset to Content-Type Header

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ setInterval(() => {
     createRssVRChatDevUpdates()
 }, 1000 * 60 * 5)
 
-
 server.get("/rss/vrchat/dev-updates", (req, rep) => {
     const file = fs.readFileSync(path.join(__dirname,'rss','vrchatDevUpdates.xml'), {encoding: 'utf-8'});
-    rep.headers({"Content-Type": "application/rss+xml"}).send(file);
+    rep.headers({"Content-Type": "application/rss+xml;charset=UTF-8"}).send(file);
 })
 
 server.listen({host: process.env.SERVER_HOST || "127.0.0.1",port: process.env.SERVER_PORT || 8080})
